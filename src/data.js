9e84b9c887fa3da2c753fde4b36d96c99eb7039e
Rakesh <nrvsrakesh.14@gmail.com>
2022-07-29 13:12:17 +0530

    video-03

---
+++ src/data.js
@@ -0,0 +1,102 @@
+const data = {
+  products: [
+    {
+      name: 'Bed 01',
+      slug: 'bed-01',
+      category: 'Beds',
+      image: '/images/p1.jpg', //679px x 829px
+      price: 6000,
+      countInStock: 5,
+      brand: 'mattrresses',
+      rating: 4.5,
+      numReviews: 5,
+      description: 'high quality wood',
+    },
+    {
+      name: 'DoubleBed 02',
+      slug: 'bed-02',
+      category: 'Beds',
+      image: '/images/p2.jpg', //679px x 829px
+      price: 10000,
+      countInStock: 5,
+      brand: 'wakefit',
+      rating: 4.5,
+      numReviews: 5,
+      description: 'high quality wood',
+    },
+    {
+      name: 'drawer 01',
+      slug: 'drawer-01',
+      category: 'drawers',
+      image: '/images/p3.jpg', //679px x 829px
+      price: 2000,
+      countInStock: 10,
+      brand: 'mattrresses',
+      rating: 4,
+      numReviews: 5,
+      description: 'high quality wood',
+    },
+    {
+      name: 'drawer 02',
+      slug: 'drawer-02',
+      category: 'drawers',
+      image: '/images/p4.jpg', //679px x 829px
+      price: 2500,
+      countInStock: 10,
+      brand: 'wakefit',
+      rating: 4,
+      numReviews: 5,
+      description: 'high quality wood and with doors',
+    },
+    {
+      name: 'Rapri Clip Led Light',
+      slug: 'rapri-clip-led-light',
+      category: 'Speciaity Lightening',
+      image: '/images/p5.jpg', //679px x 829px
+      price: 439,
+      countInStock: 10,
+      brand: 'Zentec',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Wonderful Decoration Add Something Special To Your Bedroom, Room, College Dorm, Wedding, Party. Easy to Use Just hang it on the wall. No mounting tools needed, suitable for art works or crafts, prints, cards etc.',
+    },
+    {
+      name: 'hanging lights',
+      slug: 'hanging-lights',
+      category: 'Speciaity Lightening',
+      image: '/images/p6.jpg', //679px x 829px
+      price: 489,
+      countInStock: 10,
+      brand: 'Zentec',
+      rating: 4.0,
+      numReviews: 10,
+      description: '',
+    },
+    {
+      name: 'wall Light',
+      slug: 'wall-light',
+      category: 'Speciaity Lightening',
+      image: '/images/p7.jpg', //679px x 829px
+      price: 899,
+      countInStock: 10,
+      brand: 'Zentec',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'gives a bright light and color to the wall',
+    },
+    {
+      name: 'hallway runway',
+      slug: 'hallway-runway',
+      category: 'runway',
+      image: '/images/p8.jpg', //679px x 829px
+      price: 349,
+      countInStock: 10,
+      brand: 'Zebtec',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'makes the runway good and absorbs dust',
+    },
+  ],
+};
+export default data;
